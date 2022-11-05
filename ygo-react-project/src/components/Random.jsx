a11020d97e585f64ee78fc12432bb20c0fb79ecc
Andrew <zhengandrew35@gmail.com>
2022-11-05 13:08:07 -0400

    added random card button

--- ygo-react-project/src/components/Random.jsx
+++ ygo-react-project/src/components/Random.jsx
@@ -1,18 +1,18 @@
 import{ useState, useEffect } from 'react'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+
 
 
 export default function Random(){
     const[isRandom,setIsRandom] = useState(null)
 
+    const getRandom= async()=>{
+        const response =await axios.get('https://db.ygoprodeck.com/api/v7/randomcard.php')
+        setIsRandom(response.data)
+      
+    }
         useEffect(()=>{
 
-            const getRandom= async()=>{
-                const response =await axios.get('https://db.ygoprodeck.com/api/v7/randomcard.php')
-                setIsRandom(response.data)
-              
-            }
             getRandom()
             
         },[])
@@ -24,7 +24,7 @@ export default function Random(){
             return(
            <div>
             
-            
+            <button onClick={getRandom}>random</button>
             <h2>{isRandom.name}</h2>
             <img src={isRandom.card_images[0].image_url}/>
 
